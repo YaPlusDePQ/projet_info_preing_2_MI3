4bff3bf6cff7a708924b8f5192248e585b0a3377
zeyneb <zeineb.chaabane98@gmail.com>
2023-02-01 19:30:37 +0100

    new main-last version

--- src/sh/main.sh
+++ src/sh/main.sh
@@ -1,123 +1,178 @@
-#!/bin/bash
-#changing the coloumn $10 into two coloumns 
-sed 's/,/;/g' meteo_filtered_data_v1.csv > tmp.csv 
-#-----------------DATA------------------------------
-a=`cat meteo_filtered_data_v1.csv | wc -l`
-while getopts "mhwt:p:" opt
+#!/bin/bash 
+
+
+for arg in $@
 do 
-    case $opt in
-    h) #height 
-        cat tmp0.csv | cut -d ';' -f1,10,11,15 --output-delimiter ';' > tmp.csv
-        head -n1 tmp.csv > height.csv
-        tail -n$((a-1)) tmp.csv >> height.csv
-        rm tmp.csv
-    ;;
-    m) # moisture 
-        cat tmp0.csv | cut -d ';' -f1,6,10,11 --output-delimiter ';'  > tmp1.csv
-        head -n1 tmp1.csv > moisture.csv 
-        tail -n$((a-1)) tmp1.csv >> moisture.csv 
-        rm tmp1.csv
-    ;;
-    w) #wind 
-        cat tmp0.csv | cut -d ';' -f1,4,5,10,11 --output-delimiter ';' > tmp1.csv
-        head -n1 tmp1.csv > wind.csv
-        tail -n$((a-1)) tmp1.csv >> wind.csv
-        rm tmp1.csv
-    ;;
-    t) #temperature 
-    mode=$OPTARG
-    if (($mode==1)) # mode 1 : max, min and average temperatures
-    then 
-        cat tmp0.csv | cut -d ';' -f1,12,13,14 --output-delimiter ';'  > tmp1.csv
-        head -n1 tmp1.csv > temperature1.csv 
-        tail -n$((a-1)) tmp1.csv >> temperature1.csv 
-        rm tmp1.csv
-    elif (($mode==2)) # mode 2 :
-        cat tmp0.csv | cut -d ';' -f2,12 --output-delimiter ';'  > tmp1.csv 
-        head -n1 tmp1.csv > temperature2.csv
-        tail -n$((a-1)) tmp1.csv >> temperature2.csv
-        rm tmp1.csv
-    then
-    else #mode 3 : temperatures per date/hour 
-        cat tmp0.csv  | cut -d ';' -f1,2,12 --output-delimiter ';'  > tmp1.csv 
-        head -n1 tmp1.csv > temperature3.csv
-        tail -n$((a-1)) tmp1.csv >> temperature3.csv
-        rm tmp1.csv
-    fi
-    ;;
-    p) #pression 
-    mode=$OPTARG
-    if (($mode==1)) # mode 1 : max, min and average pressions 
-    then 
-        cat tmp0.csv | cut -d ';' -f1,3,7,8 --output-delimiter ';'  > tmp1.csv 
-        head -n1 tmp1.csv > pression1.csv
-        tail -n$((a-1)) tmp1.csv >> pression1.csv
-        rm tmp1.csv
-    elif (($mode==2)) # mode 2 :
-        cat tmp0.csv | cut -d ';' -f2,7 --output-delimiter ';'  > tmp1.csv 
-        head -n1 tmp1.csv > pression2.csv
-        tail -n$((a-1)) tmp1.csv >> pression2.csv
-        rm tmp1.csv
-    then
-    else #mode 3 : pressions per date/hour 
-        cat tmp0.csv | cut -d ';' -f1,2,7 --output-delimiter ';'  > tmp1.csv 
-        head -n1 tmp1.csv > pression3.csv
-        tail -n$((a-1)) tmp1.csv >> pression3.csv
-        rm tmp1.csv
-    fi
-    ;;
-    \?) echo "ERREUR! L'option saisie n'existe pas"
+    case $arg in 
+        --abr)
+        sortn='abr'
+        ;;
+        --avl)
+        sortn= 'avl'
+        ;;
+        --tab)
+        sortn='tab'
+        ;;
+        --help)
+        cat man.txt
+        exit 1
+        ;;
+        \?) echo "ERROR"
     esac
 done
-
-#-----------------LOCATIONS---------------------------
-LOCK=0
-while getopts "FGSAOQ" opt
+#initialization
+arg_F=0
+arg_A=0
+arg_S=0
+arg_Q=0
+arg_G=0
+arg_O=0
+arg_h=0
+arg_m=0
+arg_w=0
+arg_t=0
+arg_p=0
+arg_d=0
+while getopts "FAGOQShmwf:t:p:" opt
 do 
-    case $opt in 
-    F) #france + corse
-    head -n1 tmp0.csv > france.csv
-    awk -F ";" '41<=$10 && $10<=52 && -5.5<=$11 && $11<=9.4 {print $0;}' tmp0.csv   >> france.csv
-    LOCK=1
+case $opt in 
+    f) 
+        a=$OPTARG
+        b=$OPTARG
     ;;
-    G) #guyane francaise
-    head -n1 tmp0.csv > guyane.csv
-    awk -F ";" '2.1<=$10 && $10<=5.9 && -54.7<=$11 && $11<=-51 {print $0}' tmp0.csv >> guyane.csv
-    LOCK=1
+    F)arg_F=1
     ;;
-    S) #saint pierre et miquelon : est du canada
-    head -n1 tmp0.csv > saint_pierre.csv
-    awk -F ";" '46.6<=$10 && $10<=47.1 && -56.5<=$11 && $11<=-56 {print $0}' tmp0.csv >> saint_pierre.csv
-    LOCK=1
+    A) arg_A=1
     ;;
-    A) # Antilles
-    head -n1 tmp0.csv> Antilles.csv
-    awk -F ";" '9.5<=$10 && $10<=22.1 && -76.03<=$11 && $11<=-53.03 {print $0}' tmp0.csv >> Antilles.csv
-    LOCK=1
+    G) arg_G=1
     ;;
-    O) #ocean indien 
-    head -n1 tmp0.csv > oc_indien.csv
-    awk -F ";" '-60.6<=$10 && $10<=8.16 && 28.3<=$11 && $11<=132.9 {print $0}' tmp0.csv >> oc_indien.csv
-    LOCK=1
+    O) arg_O=1
     ;;
-    Q) #antartique 
-    head -n1 tmp0.csv > antartique.csv
-
-    awk -F ";" '-63.5<=$10 && $10<=-57.6 && -172<=$11 && $11<=17.4 {print $0}'  tmp0.csv >> antartique.csv
-    LOCK=1
+    Q)arg_Q=1
     ;;
-    \?) echo "ERREUR! l'option lieu n'existe pas "
+    S)arg_S=1
+    ;;
+    h)arg_h=1
+    ;;
+    m)arg_m=1
+    ;;
+    w)arg_w=1
+    ;;
+    t)arg_t=1
+    mode=$OPTARG
+    ;;
+    p)arg_p=1
+    mode=$OPTARG
+    ;;
+    \?) echo "ERROR"
     esac
-    if [$LOCK -eq 1]
-    then 
-        break # PAS sure la !!!
-    else 
-        continue 
-    fi 
 done 
-#dates
+sed 's/,/;/g' $a > tmp0.csv #Splitting column $10 into two columns 
+a="tmp0.csv"
+sed 's/,/;/g' $a > tmp1.csv #Splitting column $10 into two columns
+b="tmp1.csv"
+#------------------------------LOCATIONS----------------------------------------------------
+if (($arg_A == 1 ))#--------------------------------ANTILLES------------------------------------
+then
+    head -n1 $a > Antilles.csv
+    awk -F ";" '9.5<=$10 && $10<=22.1 && -76.03<=$11 && $11<=-53.03 {print $0}' $a >> Antilles.csv
+    b="Antilles.csv"   
+elif (($arg_F == 1))#-----------------FRANCE METROPOLITAINE & CORSE--------------------------------
+then 
+    awk -F ";" '41<=$10 && $10<=52 && -5.5<=$11 && $11<=9.4 {print $0;}' $a   > france.csv
+    b="france.csv"
+elif (($arg_G == 1)) #---------------------GUYANE FRANCAISE---------------------------------------
+then 
+    awk -F ";" '2.1<=$10 && $10<=5.9 && -54.7<=$11 && $11<=-51 {print $0}' $a  > guyane.csv
+    b="guyane.csv"
+elif (($arg_S == 1))#-----------------------SAINT-PIERRE ET MIQUELON --------------------------
+then 
+    awk -F ";" '46.6<=$10 && $10<=47.1 && -56.5<=$11 && $11<=-56 {print $0}' $a  > saint_pierre.csv
+    b="saint_pierre.csv"
+elif (($arg_O == 1))#---------------------------OCEAN INDIEN---------------------------
+then
 
-#--------------TRIS-------------------
-#-------------Files-------------------
-#------------MAN---------------------
+    awk -F ";" '-60.6<=$10 && $10<=8.16 && 28.3<=$11 && $11<=132.9 {print $0}' $a  > oc_indien.csv
+    b="oc_indien.csv"
+elif (($arg_Q == 1))#------------------------------ANTARTIQUE----------------------
+then 
+    awk -F ";" '-63.5<=$10 && $10<=-57.6 && -172<=$11 && $11<=17.4 {print $0}'  $a  > antartique.csv
+    b="antartique.csv"
+
+fi 
+#-------------DATA-------------------------
+if (($arg_h == 1)) #----------------------height------------------------
+then 
+    cut -d ';' -f1,10,11,15 --output-delimiter ';' $b > tmp.csv
+    nt=`cat $b | wc -l`
+    echo "ID OMM station;Latitude;Longitude;Altitude" > height.csv
+    tail -n$((nt-1)) tmp.csv >> height.csv
+    rm tmp.csv
+fi  
+if (($arg_m == 1))#-----------------------moisture---------------------
+then 
+    cut -d ';' -f1,6,10,11 --output-delimiter ';' $b  > tmp.csv
+    nt=`cat $b | wc -l`
+    echo "ID OMM station;Humidité;Latitude;Longitude" > moisture.csv
+    tail -n$((nt-1)) tmp.csv >> moisture.csv
+    rm tmp.csv
+fi  
+if (($arg_w == 1))#------------------------wind----------------------
+then 
+    cut -d ';' -f1,4,5,10,11 --output-delimiter ';' $b > tmp.csv
+    nt=`cat $b | wc -l`
+    echo "ID OMM station;Direction du vent moyen 10 mn;Vitesse du vent moyen 10 mn;Latitude;Longitude" > wind.csv
+    tail -n$((nt-1)) tmp.csv >> wind.csv
+    rm tmp.csv
+fi  
+if (($arg_t == 1)) #--------------------Temperature--------------------
+then 
+    if (($mode == 1)) #-------------------mode 1------------------------
+    then 
+        cut -d ';' -f1,12 --output-delimiter ';' $b  > tmp.csv
+        nt=`cat $b | wc -l`
+        echo "ID OMM station;Température (°C)" > temperature1.csv
+        tail -n$((nt-1)) tmp.csv >> temperature1.csv 
+        rm tmp.csv
+    elif (($mode == 2)) #----------------mode 2----------------
+    then
+        cut -d ';' -f2,12 --output-delimiter ';' $b  > tmp.csv 
+        nt=`cat $b | wc -l`
+        echo "Date;Température (°C)" > temperature2.csv
+        tail -n$((nt-1)) tmp.csv >> temperature2.csv 
+        rm tmp.csv
+    
+    else #------------------------------mode 3--------------------------
+        cut -d ';' -f1,2,12 --output-delimiter ';' $b > tmp.csv 
+        nt=`cat $b | wc -l`
+        echo "ID OMM station;Date;Température (°C)" > temperature3.csv
+        tail -n$((nt-1)) tmp.csv >> temperature3.csv 
+        rm tmp.csv
+    fi
+fi  
+if (($arg_p == 1))#----------------------Pression---------------------
+then 
+    if (($mode == 1)) #---------------------mode 1---------------- 
+    then 
+        cut -d ';' -f1,3,7,8 --output-delimiter ';'  $b > tmp.csv 
+        nt=`cat $b | wc -l`
+        echo "ID OMM station;Pression au niveau mer;Pression station;Variation de pression en 24 heures" > pression1.csv
+        tail -n$((nt-1)) tmp.csv >> pression1.csv 
+        rm tmp.csv
+    elif (($mode == 2)) #------------------mode 2----------------
+    then
+        cut -d ';' -f2,7 --output-delimiter ';' $b > tmp.csv 
+        nt=`cat $b | wc -l`
+        echo "Date;Pression station" > pression2.csv
+        tail -n$((nt-1)) tmp.csv >> pression2.csv 
+        rm tmp.csv
+    
+    else #-----------------------mode 3----------------------
+        cut -d ';' -f1,2,7 --output-delimiter ';' $b  > tmp.csv 
+        nt=`cat $b | wc -l`
+        echo "ID OMM station;Date;Pression station" > pression3.csv
+        tail -n$((nt-1)) tmp.csv >> pression3.csv 
+        rm tmp.csv
+    fi
+fi  
 exit 0
