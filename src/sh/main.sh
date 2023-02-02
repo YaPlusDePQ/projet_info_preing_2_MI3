d51ff57f1b7002babccf0836fde276737d583d52
zeyneb <zeineb.chaabane98@gmail.com>
2023-02-02 19:56:36 +0100

    updates

--- src/sh/main.sh
+++ src/sh/main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+make -f makefile
 #initialization
 arg_F=0
 arg_A=0
@@ -13,7 +14,7 @@ arg_t=0
 arg_p=0
 arg_d=0
 arg_f=0
-
+arg_r=0
 while [ $# -ne 0 ]
 do 
     case $1 in
@@ -88,6 +89,10 @@ do
             shift
             shift
             ;;
+        -r)
+            arg_r=1
+            shift
+            ;;
         *) echo "ERROR! Command not found($1). Use command --help for the man"
             exit 1
             ;;
@@ -104,31 +109,31 @@ sed 's/,/;/g' $a > tmp0.csv #Splitting column $10 into two columns
 a="tmp0.csv"
 sed 's/,/;/g' $a > tmp1.csv #Splitting column $10 into two columns
 b="tmp1.csv"
-#------------------------------LOCATIONS----------------------------------------------------
+#--------------------------------------------------LOCATIONS----------------------------------------------------
 if (($arg_A == 1 ))#--------------------------------ANTILLES------------------------------------
 then
     awk -F ";" '9.5<=$10 && $10<=22.1 && -76.03<=$11 && $11<=-53.03 {print $0}' $a >> Antilles.csv
     b="Antilles.csv"   
 elif (($arg_F == 1))#-----------------FRANCE METROPOLITAINE & CORSE--------------------------------
 then 
-    awk -F ";" '41<=$10 && $10<=52 && -5.5<=$11 && $11<=9.4 {print $0;}' $a   > france.csv
+    awk -F ";" ' 41<=$10 && $10<=52 && -5.5<=$11 && $11<=9.4 {print $0;} ' $a   > france.csv
     b="france.csv"
 elif (($arg_G == 1)) #---------------------GUYANE FRANCAISE---------------------------------------
 then 
-    awk -F ";" '2.1<=$10 && $10<=5.9 && -54.7<=$11 && $11<=-51 {print $0}' $a  > guyane.csv
+    awk -F ";" ' 2.1<=$10 && $10<=5.9 && -54.7<=$11 && $11<=-51 {print $0} ' $a  > guyane.csv
     b="guyane.csv"
 elif (($arg_S == 1))#-----------------------SAINT-PIERRE ET MIQUELON --------------------------
 then 
-    awk -F ";" '46.6<=$10 && $10<=47.1 && -56.5<=$11 && $11<=-56 {print $0}' $a  > saint_pierre.csv
+    awk -F ";" ' 46.6<=$10 && $10<=47.1 && -56.5<=$11 && $11<=-56 {print $0} ' $a  > saint_pierre.csv
     b="saint_pierre.csv"
 elif (($arg_O == 1))#---------------------------OCEAN INDIEN---------------------------
 then
 
-    awk -F ";" '-60.6<=$10 && $10<=8.16 && 28.3<=$11 && $11<=132.9 {print $0}' $a  > oc_indien.csv
+    awk -F ";" ' -60.6<=$10 && $10<=8.16 && 28.3<=$11 && $11<=132.9 {print $0} ' $a  > oc_indien.csv
     b="oc_indien.csv"
 elif (($arg_Q == 1))#------------------------------ANTARTIQUE----------------------
 then 
-    awk -F ";" '-63.5<=$10 && $10<=-57.6 && -172<=$11 && $11<=17.4 {print $0}'  $a  > antartique.csv
+    awk -F ";" ' -63.5<=$10 && $10<=-57.6 && -172<=$11 && $11<=17.4 {print $0} '  $a  > antartique.csv
     b="antartique.csv"
 
 fi 
@@ -140,7 +145,32 @@ then
     echo "ID OMM station;Latitude;Longitude;Altitude" > height.csv
     tail -n$((nt-1)) tmp.csv >> height.csv
     rm tmp.csv
-fi  
+    if ("$sortn"="abr")
+    then
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -h -r
+        else 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -h 
+        fi 
+    elif ("$sortn"="avl")
+    then 
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -h -r
+        else 
+            ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -h 
+        fi 
+    else 
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -h -r
+        else 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -h 
+        fi 
+    fi 
+fi   
+
 if (($arg_m == 1))#-----------------------moisture---------------------
 then 
     cut -d ';' -f1,6,10,11 --output-delimiter ';' $b  > tmp.csv
@@ -148,6 +178,30 @@ then
     echo "ID OMM station;Humidité;Latitude;Longitude" > moisture.csv
     tail -n$((nt-1)) tmp.csv >> moisture.csv
     rm tmp.csv
+    if ("$sortn"="abr")
+    then
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -m -r
+        else 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -m 
+        fi 
+    elif ("$sortn"="avl")
+    then 
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -m -r
+        else 
+            ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -m 
+        fi 
+    else 
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -m -r
+        else 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -m 
+        fi 
+    fi 
 fi  
 if (($arg_w == 1))#------------------------wind----------------------
 then 
@@ -156,6 +210,30 @@ then
     echo "ID OMM station;Direction du vent moyen 10 mn;Vitesse du vent moyen 10 mn;Latitude;Longitude" > wind.csv
     tail -n$((nt-1)) tmp.csv >> wind.csv
     rm tmp.csv
+    if ("$sortn"="abr")
+    then
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -w -r
+        else 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -w
+        fi 
+    elif ("$sortn"="avl")
+    then 
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -w -r
+        else 
+            ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -w
+        fi 
+    else 
+        if [$arg_r -eq 1 ]
+        then 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -w -r
+        else 
+            ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -w 
+        fi 
+    fi 
 fi  
 if (($arg_t == 1)) #--------------------Temperature--------------------
 then 
@@ -166,6 +244,31 @@ then
         echo "ID OMM station;Température (°C)" > temperature1.csv
         tail -n$((nt-1)) tmp.csv >> temperature1.csv 
         rm tmp.csv
+        if ("$sortn"="abr")
+        then
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t1 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t1
+            fi 
+        elif ("$sortn"="avl")
+        then 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t1 -r
+            else 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t1
+            fi 
+        else 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t1 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t1 
+            fi 
+        fi 
+    fi   
     elif (($mode == 2)) #----------------mode 2----------------
     then
         cut -d ';' -f2,12 --output-delimiter ';' $b  > tmp.csv 
@@ -173,13 +276,60 @@ then
         echo "Date;Température (°C)" > temperature2.csv
         tail -n$((nt-1)) tmp.csv >> temperature2.csv 
         rm tmp.csv
-    
+        if ("$sortn"="abr")
+        then
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t2 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t2
+            fi 
+        elif ("$sortn"="avl")
+        then 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t2 -r
+            else 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t2
+            fi 
+        else 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t2 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t2 
+            fi 
+        fi 
     else #------------------------------mode 3--------------------------
         cut -d ';' -f1,2,12 --output-delimiter ';' $b > tmp.csv 
         nt=`cat $b | wc -l`
         echo "ID OMM station;Date;Température (°C)" > temperature3.csv
         tail -n$((nt-1)) tmp.csv >> temperature3.csv 
         rm tmp.csv
+        if ("$sortn"="abr")
+        then
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t3 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t3
+            fi 
+        elif ("$sortn"="avl")
+        then 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t3 -r
+            else 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t3
+            fi 
+        else 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t3 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -t3
+            fi 
+        fi 
     fi
 fi  
 if (($arg_p == 1))#----------------------Pression---------------------
@@ -191,6 +341,30 @@ then
         echo "ID OMM station;Pression au niveau mer;Pression station;Variation de pression en 24 heures" > pression1.csv
         tail -n$((nt-1)) tmp.csv >> pression1.csv 
         rm tmp.csv
+        if ("$sortn"="abr")
+        then
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p1 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p1
+            fi 
+        elif ("$sortn"="avl")
+        then 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p1 -r
+            else 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p1
+            fi 
+        else 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p1 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p1 
+            fi 
+        fi 
     elif (($mode == 2)) #------------------mode 2----------------
     then
         cut -d ';' -f2,7 --output-delimiter ';' $b > tmp.csv 
@@ -198,13 +372,62 @@ then
         echo "Date;Pression station" > pression2.csv
         tail -n$((nt-1)) tmp.csv >> pression2.csv 
         rm tmp.csv
-    
+        if ("$sortn"="abr")
+        then
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p2 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p2
+            fi 
+        elif ("$sortn"="avl")
+        then 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p2 -r
+            else 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p2
+            fi 
+        else 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p2 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p2 
+            fi 
+        fi 
     else #-----------------------mode 3----------------------
         cut -d ';' -f1,2,7 --output-delimiter ';' $b  > tmp.csv 
         nt=`cat $b | wc -l`
         echo "ID OMM station;Date;Pression station" > pression3.csv
         tail -n$((nt-1)) tmp.csv >> pression3.csv 
         rm tmp.csv
+        if ("$sortn"="abr")
+        then
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p3 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p3
+            fi 
+        elif ("$sortn"="avl")
+        then 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p3 -r
+            else 
+                ./exec --avl -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p3
+            fi 
+        else 
+            if [$arg_r -eq 1 ]
+            then 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p3 -r
+            else 
+                ./exec --abr -f source/meteo_filtered_data_v1.csv -o PROJET_INFO_PREING2_MI3/sh/test.csv -p3 
+            fi 
+        fi 
     fi
 fi
+
+
 exit 0
