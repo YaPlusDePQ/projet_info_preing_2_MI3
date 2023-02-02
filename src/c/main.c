4d5deab803373ff5b09f8501db9e245bfb4bf565
Fiona <cleo9501@gmail.com>
2023-02-02 11:38:05 +0100

    list main

--- src/c/main.c
+++ src/c/main.c
@@ -19,17 +19,17 @@ int main(int argc, char **argv){
         case 'm':
             if(!strcmp(type, "--abr")) HeightMoistureModeABRAVL(sourcePath, outPath, MOISTUREMODE, 0, isReversed);
             if(!strcmp(type, "--avl")) HeightMoistureModeABRAVL(sourcePath, outPath, MOISTUREMODE, 1, isReversed);
-            else //here moisture mode tab;
+            else Moisture(sourcePath, outPath,isReversed);
             break;
         case 'h':
             if(!strcmp(type, "--abr"))HeightMoistureModeABRAVL(sourcePath, outPath, HEIGHTMODE, 0, !isReversed);
             if(!strcmp(type, "--avl"))HeightMoistureModeABRAVL(sourcePath, outPath, HEIGHTMODE, 1, !isReversed);
-            else;
+            else Height(sourcePath, outPath, isReversed);
             break;
         case 'w':
             if(!strcmp(type, "--abr"))WindModeABRAVL(sourcePath, outPath, 0, isReversed);
             if(!strcmp(type, "--avl"))WindModeABRAVL(sourcePath, outPath, 1, isReversed);
-            else;
+            else Wind(sourcePath, outPath, isReversed);
             break;
         case 't':
         case 'p':
@@ -37,17 +37,17 @@ int main(int argc, char **argv){
                 case '1':
                     if(!strcmp(type, "--abr"))TempPressureModeABRAVL(sourcePath, outPath, 0, 1, isReversed);
                     if(!strcmp(type, "--avl"))TempPressureModeABRAVL(sourcePath, outPath, 1, 1, isReversed);
-                    else;
+                    else Option1(sourcePath, outPath, isReversed);
                     break;
                 case '2':
                     if(!strcmp(type, "--abr"))TempPressureModeABRAVL(sourcePath, outPath, 0, 2, isReversed);
                     if(!strcmp(type, "--avl"))TempPressureModeABRAVL(sourcePath, outPath, 1, 2, isReversed);
-                    else;
+                    else Option2(sourcePath, outPath, isReversed);
                     break;
                 case '3':
                     if(!strcmp(type, "--abr"))TempPressureModeABRAVL(sourcePath, outPath, 0, 3, isReversed);
                     if(!strcmp(type, "--avl"))TempPressureModeABRAVL(sourcePath, outPath, 1, 3, isReversed);
-                    else;
+                    else Option3(sourcePath, outPath, isReversed);
                     break;
             }
             break;
