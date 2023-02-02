de0893326448f0da67860ca69e7378302d57eb55
Fiona <cleo9501@gmail.com>
2023-02-02 12:10:17 +0100

    rectifications

--- src/c/List/ListCalculus.h
+++ src/c/List/ListCalculus.h
@@ -11,9 +11,4 @@ float Average(List * plist);                //
 
 float Average3(List * plist);               //Takes a second list with only one station and all it's values to fin the average (in the 3rd row)
 
-int monthToDay(int month);                  //Converts months into days
-
-long long int dateToInt(const char* date);  //Converts a date in this format "YYYY-MM-DDTHH:00:00+UTC:00" into a long long int
-
-
 #endif
